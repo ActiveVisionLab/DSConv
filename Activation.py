8f8fdcc4d99a8856d27619f27328ddda5e77d042
marcelo <marcelo@robots.ox.ac.uk>
2018-12-14 15:59:14 +0000

    Before doing the -4->+3 trick with negative values in the bonus multiply

--- Activation.py
+++ Activation.py
@@ -26,110 +26,37 @@ def to_block(blocks, mant=5):
     #        blocks[:, h, l] = quantize(blocks[:, h, l])
     return blocks
 
-def quantize(activations, mant=5):
+def quantize(activations, EXPONENT_WIDTH=4, MANTISSA_WIDTH=7):
     # This receives an array of shape:
     # [channel, bs_size, h, w]
     #print(activations[1, 0:32, 1, 1])
-    absAct = torch.abs(activations)
-    logAbs = torch.log2(absAct)
-    intlogAbs = logAbs.type(torch.cuda.IntTensor)
-    maxlogAbs, _ = torch.max(intlogAbs, dim=1)
-    value = to_nearest(activations, maxlogAbs)
-    return value
+    int_log = find_exponent(activations, EXPONENT_WIDTH)
+    max_exponent = find_max_exponent(int_log)
+    quantized_activations = to_exponent_mantissa_width(activations, max_exponent, MANTISSA_WIDTH)
+    return quantized_activations
 
-def to_nearest(array, maxlog, mant=5):
-    # This receives arrays of size:
-    # [channel, bs_size, h, w]
-    # [channel, h, w]
+def find_exponent(array, EXPONENT_WIDTH):
+    MAX = 2**(EXPONENT_WIDTH-1)-1
+    MIN = -2**(EXPONENT_WIDTH-1)
+    absolute = torch.abs(array)
+    value_log = torch.log2(absolute)
+    value_log = torch.clamp(value_log, MIN, MAX)
+    int_log = torch.floor(value_log)
+    return int_log
+
+def find_max_exponent(array):
+    max_exponent, _ = torch.max(array, dim=1)
+    return max_exponent
+
+def to_exponent_mantissa_width(array, maxlog, MANTISSA_WIDTH):
+    shp = array.shape
     maxlog = maxlog.unsqueeze(1)
-    maxlog = torch.clamp(maxlog, -10, 10) # To avoid inf
-    maxlog = maxlog.expand(array.shape[0], array.shape[1], array.shape[2], array.shape[3])
-    mantT = 5*torch.ones(array.shape).type(torch.cuda.IntTensor)
-    diff = mantT-maxlog+1
-    scale = torch.pow(2, diff).type(torch.cuda.FloatTensor)
-    array = array*scale
-    array = torch.clamp(array, -2**8, 2**10)
+    # NOTE THAT THIS -1 IS BECAUSE OF THE LEADING 1
+    exponent_needed = (MANTISSA_WIDTH-maxlog-1)*torch.ones(shp).cuda()
+    first_mant_w = torch.pow(2, exponent_needed)
+    array = array*first_mant_w
+    # Half LSB rounding:
     array = torch.round(array)
-    array = array.type(torch.cuda.FloatTensor)
-    #print("INT Value:", array[1, 0:32, 1, 1])
-    array = array/scale
-    #print("Quantized:",  array[1, 0:32, 1, 1])
-    #print("")
+    array = array/first_mant_w
+
     return array
-
-def transform_activation2(tensor, mantissa, exponent, blk):
-    """
-    This is a block floating point implementation
-    Each block will share an exponent. We first pick the largest exponent and shift the rest
-    :params: tensor is the tensor to be converted
-    :params: mantissa is the number of bits that the mantissa will have
-    :params: exponent is the number of bits that the shared exponent will have
-    :params: blk is the block size of the tensor
-    :return: tensor in block floating point format
-    """
-
-
-    # Just converting the tensor from torch to numpy
-    shp = tensor.shape
-    number_of_blocks = math.ceil(shp[1]/blk)
-    # Loop through every block
-    tensor = torch.clamp(tensor, -1, 4)
-    precision = 1024
-    tensor = precision*tensor
-    tensor = tensor.to(torch.int32)
-    tensor = tensor.to(torch.float32)
-    tensor = tensor/precision
-    #for w in range(shp[3]):
-    #    for h in range(shp[2]):
-    #        for c in range(shp[0]):
-    #            for blk in range(number_of_blocks):
-
-    #                if blk == number_of_blocks-1:
-    #                    block = tensor[c, blk*number_of_blocks:, w, h]
-    #                else:
-    #                    block = tensor[c, blk*number_of_blocks:(blk+1)*number_of_blocks, w, h]
-    #                block = quantize_block(block, 3, 3)
-
-    #                if blk == number_of_blocks-1:
-    #                    tensor[c, blk*number_of_blocks:, w, h] = block
-    #                else:
-    #                    tensor[c, blk*number_of_blocks:(blk+1)*number_of_blocks, w, h] = block
-    return tensor
-
-def quantize_block(block, mantissa, exponent):
-    """Given a block, this function chooses the biggest exponent value and shifts all other values as in Block Floating Point
-
-    :block: numpy array of the values in the block
-    :mantissa: number of bits for the mantissa
-    :exponent: number of bits for the exponent
-    :returns: quantized block according to mantissa and exponent
-    """
-    #maximum = max(abs(block))
-    #print(block)
-    #values = generate_mantissa(mantissa, exponent)
-    #print(values)
-    #input('')
-    precision= 10
-    block = precision*block
-    block = block.to(torch.int32)
-    block = block.to(torch.float32)
-    block = block/precision
-    return block
-
-def generate_mantissa(mantissa, exponent):
-    """This method gets a mantissa and exponent number of bits and generates a
-    list with all possible values
-
-    :exponent: Number of bits for the exponent
-    :mantissa: Number of bits for the mantissa
-    :returns: List with all possible values for the mantissa
-
-    """
-    values = []
-    for i in range(2**exponent):
-        if i == 0:
-            new_values = [(x/2**mantissa) for x in range(2**mantissa)]
-        else:
-            new_values = [2**(i-1)*(1+(x/(2**mantissa))) for x in range(2**mantissa)]
-        values.extend(new_values)
-    return values
