f02ed2e3444674a4bc74e993468b77d18d24bff6
marcelo <marcelo@robots.ox.ac.uk>
2018-12-17 09:29:01 +0000

    Last commit before trying the -4->+3 trick to improve accuracy of DSConv

--- Activation.py
+++ Activation.py
@@ -12,24 +12,14 @@ def transform_activation(tensor, mantissa, exponent, blk):
     number_of_blocks = math.ceil(shp[1]/blk)
     for i in range(number_of_blocks):
         if i == number_of_blocks-1:
-            tensor[:, i*blk:, ...] = to_block(tensor[:, i*blk:, ...])
+            tensor[:, i*blk:, ...] = quantize(tensor[:, i*blk:, ...])
         else:
-            tensor[:, i*blk:(1+i)*blk, ...] = to_block(tensor[:, i*blk:(1+i)*blk, ...])
+            tensor[:, i*blk:(1+i)*blk, ...] = quantize(tensor[:, i*blk:(1+i)*blk, ...])
     return tensor
 
-def to_block(blocks, mant=5):
-    # This receives a shape of the array of:
-    # [channel, bs_size, h, w]
-    blocks = quantize(blocks)
-    #for h in range(blocks.shape[1]):
-    #    for l in range(blocks.shape[2]):
-    #        blocks[:, h, l] = quantize(blocks[:, h, l])
-    return blocks
-
-def quantize(activations, EXPONENT_WIDTH=4, MANTISSA_WIDTH=7):
+def quantize(activations, EXPONENT_WIDTH=4, MANTISSA_WIDTH=4):
     # This receives an array of shape:
     # [channel, bs_size, h, w]
-    #print(activations[1, 0:32, 1, 1])
     int_log = find_exponent(activations, EXPONENT_WIDTH)
     max_exponent = find_max_exponent(int_log)
     quantized_activations = to_exponent_mantissa_width(activations, max_exponent, MANTISSA_WIDTH)
@@ -51,12 +41,13 @@ def find_max_exponent(array):
 def to_exponent_mantissa_width(array, maxlog, MANTISSA_WIDTH):
     shp = array.shape
     maxlog = maxlog.unsqueeze(1)
-    # NOTE THAT THIS -1 IS BECAUSE OF THE LEADING 1
-    exponent_needed = (MANTISSA_WIDTH-maxlog-1)*torch.ones(shp).cuda()
+    # NOTE THAT THIS -2 IS BECAUSE OF THE LEADING 1 AND THE FACT THAT THIS SHOULD BE IN 2s COMPLEMENT
+    exponent_needed = (MANTISSA_WIDTH-maxlog-2)*torch.ones(shp).cuda()
     first_mant_w = torch.pow(2, exponent_needed)
     array = array*first_mant_w
     # Half LSB rounding:
     array = torch.round(array)
+    # print(array[0, :, 0, 0]) # Uncomment to print integer values
     array = array/first_mant_w
 
     return array
